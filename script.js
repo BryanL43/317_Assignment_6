c510d3efaa5bf40ec664334a523e78a61e282b8b
Bryan <bryanlee56098@gmail.com>
2024-04-19 19:27:11 -0700

    Added table and fix to string regex stuff

--- script.js
+++ script.js
@@ -15,10 +15,21 @@ document.addEventListener("DOMContentLoaded", function() {
         let unsortedFreq = {};
 
         let text = textarea.value;
-        text = text.split(/\s+/);
+        //Remove all punctuations, split by whitespace, and lowercase all word for comparison test
+        text = text.replace(/[^\w\s]|_/g, "").split(/\s+/).map(word => word.toLowerCase());
 
         for (let i = 0; i < text.length; i++) {
             let word = text[i];
+            if (/^\s*$/.test(word)) {
+                //Delete table if word is only whitespace
+                let tableToRemove = document.getElementById("root").getElementsByTagName("table")[0];
+
+                if (tableToRemove) {
+                    tableToRemove.parentNode.removeChild(tableToRemove);
+                }
+                return;
+            }
+
             if (!unsortedFreq.hasOwnProperty(word)) {
                 unsortedFreq[word] = 1;
             } else {
@@ -51,7 +62,50 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         }
 
-        textarea.value = returnString;
+        //Remove existing table for update
+        let tableToRemove = document.getElementById("root").getElementsByTagName("table")[0];
+
+        if (tableToRemove) {
+            tableToRemove.parentNode.removeChild(tableToRemove);
+        }
+
+        //Add frequency table
+        const table = document.createElement("table");
+        document.getElementById("root").appendChild(table);
+
+        const thead = document.createElement("thead");
+        table.appendChild(thead);
+
+        const thead_tr = document.createElement("tr");
+        thead.appendChild(thead_tr);
+
+        const thead_th_1 = document.createElement("th");
+        thead_th_1.textContent = "word_name";
+        thead_tr.appendChild(thead_th_1);
+
+        const thead_th_2 = document.createElement("th");
+        thead_th_2.textContent = "word_frequency";
+        thead_tr.appendChild(thead_th_2);
+
+        const tbody = document.createElement("tbody");
+        table.appendChild(tbody);
+
+        for (let i = 0; i < Math.min(sortedFreqArray.length, 5); i++) {
+            const [key, value] = sortedFreqArray[i];
+            
+            //Create row contents
+            const tbody_tr = document.createElement("tr");
+            tbody.appendChild(tbody_tr);
+
+            const word = document.createElement("td");
+            word.textContent = key;
+            tbody_tr.append(word);
+            
+            const word_freq = document.createElement("td");
+            word_freq.textContent = value;
+            tbody_tr.append(word_freq);
+
+        }
     })
 
     document.getElementById("root").appendChild(button);
